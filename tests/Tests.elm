6a90843499a1eff9a225ac74e80e4cb03ba1b929
aiya000 <aiya000.develop@gmail.com>
2018-06-20 23:10:48 +0900

    :+1: Add tests for Main.update

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,10 +2,12 @@ module Tests exposing (..)
 
 import Html exposing (..)
 import Html.Attributes as Html
-import Main
+import Main as M
 import Test exposing (..)
 import Test.Html.Query as Q
 import Test.Html.Selector as S
+import TestExp exposing (..)
+import Time.DateTime as DateTime
 import Tuple exposing (first)
 
 
@@ -14,14 +16,35 @@ view =
     describe "view's"
         [ test "clock shows 00:00:00 at first" <|
             \() ->
-                Main.view (first Main.init)
+                M.view (first M.init)
                     |> Q.fromHtml
                     |> Q.find [ S.id "clock" ]
                     |> Q.has [ S.text "00:00:00" ]
         , test "image shows zunko that is on rest" <|
             \() ->
-                Main.view (first Main.init)
+                M.view (first M.init)
                     |> Q.fromHtml
                     |> Q.find [ S.id "zunko" ]
                     |> Q.has [ S.attribute <| Html.src "./src/images/rest.png" ]
+        ]
+
+
+update : Test
+update =
+    describe "update"
+        [ "increments the Model's clock a second with the Msg's timer count up"
+            => let
+                ( model, _ ) =
+                    M.update M.TimerCountUp <| first M.init
+
+                expected =
+                    DateTime.addSeconds 1 (first M.init).clock
+               in
+                model.clock === expected
+        , "resets the Model's clock with the Msg's timer reset"
+            => let
+                ( model, _ ) =
+                    M.update (M.TimerAction M.TimerReset) <| first M.init
+               in
+                model.clock === DateTime.dateTime DateTime.zero
         ]
