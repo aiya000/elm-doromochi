9ce314150b9c3cffa4333c68a6401c93f28dc7bd
aiya000 <aiya000.develop@gmail.com>
2018-06-20 22:37:54 +0900

    :+1: Add tests for Main.view

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,16 +1,27 @@
 module Tests exposing (..)
 
+import Html exposing (..)
+import Html.Attributes as Html
+import Main
 import Test exposing (..)
-import TestExp exposing (..)
+import Test.Html.Query as Q
+import Test.Html.Selector as S
+import Tuple exposing (first)
 
 
---  target modules
-
-
-all : Test
-all =
-    describe "List module Test"
-        [ "Test"
-            => (List.range 1 10 |> List.drop 3 |> List.map (\x -> x * 2) |> List.head)
-            === Just 8
+view : Test
+view =
+    describe "view's"
+        [ test "clock shows 00:00:00 at first" <|
+            \() ->
+                Main.view (first Main.init)
+                    |> Q.fromHtml
+                    |> Q.find [ S.id "clock" ]
+                    |> Q.has [ S.text "00:00:00" ]
+        , test "image shows zunko that is on rest" <|
+            \() ->
+                Main.view (first Main.init)
+                    |> Q.fromHtml
+                    |> Q.find [ S.id "zunko" ]
+                    |> Q.has [ S.attribute <| Html.src "./src/images/rest.png" ]
         ]
