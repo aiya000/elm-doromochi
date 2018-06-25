1689251ac90a810585c9bde312f3fec3679c3e0b
aiya000 <aiya000.develop@gmail.com>
2018-06-25 21:58:56 +0900

    :+1: Add tests for zunko images

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,8 +1,9 @@
 module Tests exposing (..)
 
-import Html exposing (..)
 import Html.Attributes as Html
 import Main as M
+import Main exposing (Model)
+import Nat exposing (..)
 import Test exposing (..)
 import Test.Html.Query as Q
 import Test.Html.Selector as S
@@ -11,22 +12,93 @@ import Time.DateTime as DateTime
 import Tuple exposing (first)
 
 
+initialModel : Model
+initialModel =
+    first M.init
+
+
+{-| A prescribed value for Model.intervals.workTime
+-}
+workTime : Nat
+workTime =
+    initialModel.intervals.workTime
+
+
+{-| A prescribed value for Model.intervals.restTime
+-}
+restTime : Nat
+restTime =
+    initialModel.intervals.restTime
+
+
+{-| A prescribed value for Model.intervals.cycleLength
+-}
+cycleLength : Nat
+cycleLength =
+    initialModel.intervals.cycleLength
+
+
+{-| A half head time of a working
+-}
+atWorkHead : DateTime
+atWorkHead =
+    DateTime.zero
+
+
+{-| A half tail time of a working
+-}
+atWorkTail : DateTime
+atWorkTail =
+    let
+        halfTimeOfWork =
+            (toInt workTime // 2) + 1
+    in
+        DateTime.addMinutes halfTimeOfWork atWorkHead
+
+
+{-| A rest time
+-}
+atRest : DateTime
+atRest =
+    DateTime.addMinutes (toInt workTime) DateTime.zero
+
+
+{-| A long rest time
+-}
+atLongRest : DateTime
+atLongRest =
+    let
+        oneCycleTime =
+            plus workTimel restTime
+    in
+        DateTime.addMinutes (times oneCycleTime cycleLength) DateTime.zero
+
+
 view : Test
 view =
-    describe "view's"
-        [ test "clock shows 00:00:00 at first" <|
+    let
+        -- Zunko has `img` as the src at `time`
+        zunkoHas img time =
             \() ->
-                M.view (first M.init)
-                    |> Q.fromHtml
-                    |> Q.find [ S.id "clock" ]
-                    |> Q.has [ S.text "00:00:00" ]
-        , test "image shows zunko that is on rest" <|
-            \() ->
-                M.view (first M.init)
+                M.view { initialModel | clock = time }
                     |> Q.fromHtml
                     |> Q.find [ S.id "zunko" ]
-                    |> Q.has [ S.attribute <| Html.src "./src/images/rest.png" ]
-        ]
+                    |> Q.has [ S.attribute <| Html.src img ]
+    in
+        describe "view"
+            [ test "shows the clock as 00:00:00 at first" <|
+                \() ->
+                    M.view (first M.init)
+                        |> Q.fromHtml
+                        |> Q.find [ S.id "clock" ]
+                        |> Q.has [ S.text "00:00:00" ]
+            , test "shows the image with zunko that takes a rest time, at the rest time" <|
+                zunkoHas "./src/images/rest.png" atRest
+            , test "shows the image with zunko that works (1), at a head of the working time" <|
+                zunkoHas "./src/images/work1.png" atWorkHead
+            , test "shows the image with zunko that works (2), at a tail of the working time" <|
+                zunkoHas "./src/images/work2.png" atWorkTail
+            ]
 
 
 update : Test
